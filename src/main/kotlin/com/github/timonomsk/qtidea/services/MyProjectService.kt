94b6fccd4877545e89634f011f210d16472740f8
GitHub Action <action@github.com>
2022-02-15 00:53:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/timonomsk/qtidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/timonomsk/qtidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.timonomsk.qtidea.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.timonomsk.qtidea.MyBundle
 
 class MyProjectService(project: Project) {
 
